1578a3de9f27c671aa23684f047b47ba87e56ac4
cirrus03 <selenachang.y@gmail.com>
2026-03-20 21:09:52 -0700

    yt attmpet

--- backend/routes/youtube.js
+++ backend/routes/youtube.js
@@ -1,5 +1,25 @@
 
+require("dotenv").config( { path: './.env' } );
+const express = require("express");
+const router = express.Router();
+
+const playlistID = PLgzTt0k8mXzEk586ze4BjvDXR7c-TUSnx;
 
 //youtube api key
 // AIzaSyBEBgEcLutsfhv07UCKvFmnqYFAok4c7cs
+
+//fech yt api key function
+const getAPIKey = () => {
+    const key = process.env.YT_API_KEY;
+    console.log(key);
+    return key;
+}
+
+router.get("/playlist", async (req, res) => {
+    try {
+
+    } catch(err) {
+
+    }
+});
 
